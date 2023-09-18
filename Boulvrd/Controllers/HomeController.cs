c0eb46efb140c605ffb4e1e569ba0c122f35fc3b
ayselaa <ayselaa@code.edu.az>
2023-09-18 11:16:21 +0400

    update

---
+++ Boulvrd/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Boulvrd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
